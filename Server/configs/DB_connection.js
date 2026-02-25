445fc31ee415537bea9cb12c8094148ea2d29960
lahirulakmal23 <lahirulakmal893@gmail.com>
2026-02-25 21:36:24 +0530

    add dns promises

--- Server/configs/DB_connection.js
+++ Server/configs/DB_connection.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import { setServers } from "node:dns/promises";
+setServers(["1.1.1.1", "8.8.8.8"]);
 
 
 const envFound = dotenv.config();
 if (envFound.error) {
   throw new Error("Couldn't find .env file");
 }
+
 
 const ConnectDB = async () => {
   try {
